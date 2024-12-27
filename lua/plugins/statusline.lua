004ff0c5f22de33fc444bb79cff93e95e676a6c3
tuilad01 <thelegendaglx@gmail.com>
2024-12-27 15:49:06 +0700

    Update statusline.lua

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,4 +1,19 @@
 return {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require('lualine').setup({
+            sections = {
+                lualine_x = {
+                    'encoding',
+                    function ()
+                        local cwd = vim.fn.getcwd()
+                        local workingDirectory = cwd:match("([^/\\]+)$")
+                        return workingDirectory
+                    end,
+                    'filetype'
+                }
+            }
+        })
+    end
 }
