b2c708c73f071c435ca678e75f475f70634d19fa
dat truong <thelegendaglx@gmail.com>
2024-12-28 18:28:43 +0700

    update tailwind

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,54 +1,75 @@
 
 local M = {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = "v1.x",
-    dependencies = {
-        -- LSP support
-        'neovim/nvim-lspconfig',
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = "v1.x",
+        dependencies = {
+            -- LSP support
+            'neovim/nvim-lspconfig',
 
-        -- Autocompletetion
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lua',
-        'ray-x/lsp_signature.nvim', -- Show inline documentation
+            -- Autocompletetion
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            'ray-x/lsp_signature.nvim', -- Show inline documentation
 
-        -- command line completion
-        'nvim-lua/completion-nvim',
-        'hrsh7th/cmp-cmdline',
+            -- command line completion
+            'nvim-lua/completion-nvim',
+            'hrsh7th/cmp-cmdline',
 
 
 
-        -- Snippets
-        'L3MON4D3/LuaSnip',
-        'rafamadriz/friendly-snippets',
-        --Html auto close tag
-        'windwp/nvim-ts-autotag'
-    },
-    config = function ()
-        local cmp = require('cmp')
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                {name = 'path'},
-                {name = "cmdline", option = { ignore_cmds = { 'Man', '!'} }}
+            -- Snippets
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
+            --Html auto close tag
+            'windwp/nvim-ts-autotag'
+        },
+        config = function ()
+            
+
+            require('lsp_signature').setup({
+                bind = true,
+                floating_window = true,
+                hint_enable = true,
+                hint_prefix = "find",
+                handler_opts = {
+                    border = "rounded",
+
+                },
+                always_trigger = true,
             })
-        })
+        end
 
-        require('lsp_signature').setup({
-            bind = true,
-            floating_window = true,
-            hint_enable = true,
-            hint_prefix = "find",
-            handler_opts = {
-                border = "rounded",
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            "tailwind-tools",
+            "onsails/lspkind-nvim",
+        },
+        opts = function ()
+            local cmp = require('cmp')
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    {name = 'path'},
+                    {name = "cmdline", option = { ignore_cmds = { 'Man', '!'} }}
+                })
+            })
 
-            },
-            always_trigger = true,
-        })
-    end
+            return {
+                formatting = {
+                    format = require("lspkind").cmp_format({
+                        before = require("tailwind-tools.cmp").lspkind_format
+                    }),
+                },
+            }
+        end
+    }
 }
 
 return M
--- lua/plugins/tailwindcss.lua
+++ lua/plugins/tailwindcss.lua
@@ -1,24 +1,18 @@
 return {
-    {
-        "roobert/tailwindcss-colorizer-cmp.nvim",
-        lazy = true,
-        -- optionally, override the default options:
-        config = function()
-            require("tailwindcss-colorizer-cmp").setup({
-                color_square_width = 2,
-            })
-        end
+
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins", -- npm install -g neovim
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        --    "nvim-telescope/telescope.nvim", -- optional
+        --    "neovim/nvim-lspconfig", -- optional
     },
-    {
-        "luckasRanarison/tailwind-tools.nvim",
-        lazy = true,
-        name = "tailwind-tools",
-        build = ":UpdateRemotePlugins",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            --    "nvim-telescope/telescope.nvim", -- optional
-            --    "neovim/nvim-lspconfig", -- optional
-        },
-        opts = {} -- your configuration
-    }
+    opts = {}, -- your configuration
+    config = function ()
+        require("tailwind-tools").setup({
+            -- your configuration
+        })
+    end
 }
+
