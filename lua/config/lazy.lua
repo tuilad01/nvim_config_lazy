5cb2b157f1b1e7291bd11b993aceca2f207b3d7a
dat truong <thelegendaglx@gmail.com>
2025-01-01 15:29:56 +0700

    update keymap move line

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -71,6 +71,12 @@ vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 --    vim.cmd.lua('print("hello")')
 --end, {})
 
+-- Move line map
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
 
 -- Setup lazy.nvim
 require("lazy").setup({
