4b3f28864e8374723d6480c45ebe45ad8be62abc
dat truong <thelegendaglx@gmail.com>
2025-01-18 13:08:21 +0700

    update color

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,3 +1,8 @@
+local configs = {
+    colorscheme_transparent_background = true
+}
+
+
 return {
     --"askfiy/visual_studio_code",
     "catppuccin/nvim",
@@ -5,10 +10,23 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     --transparent = true,
+    
     config = function()
+        require('catppuccin').setup({
+            transparent_background = configs.colorscheme_transparent_background
+        })
         vim.g.nord_disable_background = true
         vim.cmd([[colorscheme catppuccin-mocha]])
 
+        vim.api.nvim_create_user_command('ToggleBackground', function ()
+            configs.colorscheme_transparent_background = not configs.colorscheme_transparent_background
+
+            require('catppuccin').setup({
+                transparent_background = configs.colorscheme_transparent_background
+            })
+
+            vim.cmd([[colorscheme catppuccin-mocha]])
+        end, {})
     end
 
 
